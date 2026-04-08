fb7b5ae6c4cb9a8e4ec586d16ec6606c344cecee
Khang Luong <vinhkhang2324@gmail.com>
2026-04-08 11:18:20 +0700

    feat: add central pact broker config

---
+++ pact.config.ts
@@ -0,0 +1,23 @@
+// pact.config.ts
+export const brokerConfig = {
+  brokerUrl: process.env.PACT_BROKER_URL,
+  brokerToken: process.env.PACT_BROKER_TOKEN,
+  consumerVersion: process.env.GIT_COMMIT ?? "local",
+  consumerVersionBranch: process.env.GIT_BRANCH ?? "local",
+};
+
+export const publishOptions = brokerConfig.brokerUrl
+  ? {
+      pactBrokerUrl: brokerConfig.brokerUrl,
+      pactBrokerToken: brokerConfig.brokerToken,
+      consumerVersion: brokerConfig.consumerVersion,
+      consumerVersionBranch: brokerConfig.consumerVersionBranch,
+    }
+  : undefined;
+
+export const canDeployOptions = brokerConfig.brokerUrl
+  ? {
+      pactBrokerUrl: brokerConfig.brokerUrl,
+      pactBrokerToken: brokerConfig.brokerToken,
+    }
+  : undefined;
